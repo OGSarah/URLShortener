97b1fc8e629ae4d9eaebd550446219ce078152b2
Sarah Clark <sarahireneclark@gmail.com>
2025-08-16 13:30:47 -0500

    Initial commit of URLShortener Xcode project

---
+++ URLShortener/URLShortener/URLShortenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLShortenerApp.swift
+//  URLShortener
+//
+//  Created by Sarah Clark on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct URLShortenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ URLShortener/URLShortenerTests/URLShortenerTests.swift
@@ -0,0 +1,17 @@
+//
+//  URLShortenerTests.swift
+//  URLShortenerTests
+//
+//  Created by Sarah Clark on 8/16/25.
+//
+
+import Testing
+@testable import URLShortener
+
+struct URLShortenerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
